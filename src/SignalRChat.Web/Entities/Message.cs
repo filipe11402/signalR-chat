522feb10dc45f5e6788feb2928623862709f6816
filipe11402 <filipe11402@outlook.pt>
2023-03-18 07:47:35 +0000

    created initial entities to support functionality

---
+++ src/SignalRChat.Web/Entities/Message.cs
@@ -0,0 +1,36 @@
+﻿namespace SignalRChat.Web.Entities;
+
+public sealed class Message
+{
+    public Guid Id { get; set; }
+
+    public Guid SenderId { get; set; }
+
+    public Guid ReceiverId { get; set; }
+
+    public string Content { get; set; }
+
+    public DateTime SentAt { get; set; }
+
+    public DateTime ReadAt { get; set; }
+
+    public Message()
+    {
+    }
+
+    public Message(
+        Guid id, 
+        Guid senderId,
+        Guid receiverId,
+        string content,
+        DateTime sentAt,
+        DateTime readAt)
+    {
+        Id = id;
+        SenderId = senderId;
+        ReceiverId = receiverId;
+        Content = content;
+        SentAt = sentAt;
+        ReadAt = readAt;
+    }
+}
---
+++ src/SignalRChat.Web/Entities/User.cs
@@ -0,0 +1,25 @@
+﻿namespace SignalRChat.Web.Entities;
+
+public sealed class User
+{
+    public Guid Id { get; set; }
+
+    public string Username { get; set; }
+
+    public string Password { get; set; }
+
+    public string Salt { get; set; }
+
+    private User() { }
+
+    public User(Guid id,
+        string username,
+        string password, 
+        string salt)
+    {
+        Id = id;
+        Username = username;
+        Password = password;
+        Salt = salt;
+    }
+}
