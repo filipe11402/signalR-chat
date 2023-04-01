69dbc2902e96df45d43f3d117ecbd974a0444a7b
filipe11402 <filipe11402@outlook.pt>
2023-04-01 11:09:30 +0100

    added filter to redirect to login if JWT is not present

--- src/SignalRChat.Web/Helpers/JwtAuthorizationFilter.cs
+++ src/SignalRChat.Web/Helpers/JwtAuthorizationFilter.cs
@@ -1,9 +1,13 @@
-﻿using Microsoft.AspNetCore.Mvc.Filters;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using SignalRChat.Web.Controllers;
 
 namespace SignalRChat.Web.Helpers;
 
 public class JwtAuthorizationFilter : Attribute, IAuthorizationFilter
 {
+    private static string[] _excludablePaths = new string[] { "Login" };
+
     public void OnAuthorization(AuthorizationFilterContext context)
     {
         var request = context.HttpContext.Request;
@@ -13,10 +17,15 @@ public class JwtAuthorizationFilter : Attribute, IAuthorizationFilter
             .Authorization
             .Contains("Bearer");
 
-        if (!hasBearerToken) 
+        if (_excludablePaths.Any(path => request.Path.Value.Contains(path))) { return; }
+
+        if (!hasBearerToken)
         {
-            
+            context.Result = new RedirectToActionResult("Login", "User", default);
+
+            return;
         }
+
 
         throw new NotImplementedException();
     }
